f316c57133e8a7495f86ce762cc3552468d9cf99
kyawlinnthant <kyawlinnthantkyawlinnthant@gmail.com>
2023-03-13 01:49:02 +0630

    buildSrc versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+        classpath("com.android.tools.build:gradle:${Versions.gradle}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
     }
 }
 
---
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -0,0 +1,7 @@
+object AppConfig {
+    const val compileSdk = 33
+    const val minSdk = 24
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
